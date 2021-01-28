c399dcc998def3b142c39a6158977048c4f65a9f
huisam <huisam@naver.com>
2021-01-28 23:54:54 +0900

    test: 예외 상황 테스트 추가

--- src/test/kotlin/com/huisam/kotlinweb/posts/PostsServiceTest.kt
+++ src/test/kotlin/com/huisam/kotlinweb/posts/PostsServiceTest.kt
@@ -2,10 +2,13 @@ package com.huisam.kotlinweb.posts
 
 import com.huisam.kotlinweb.posts.domain.PostsDomain
 import com.huisam.kotlinweb.posts.dto.PostsSaveRequestsDto
+import com.huisam.kotlinweb.posts.exception.EntityNotFoundException
 import org.assertj.core.api.BDDAssertions.then
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.concurrent.ThreadLocalRandom
 import javax.transaction.Transactional
 
 @Transactional
@@ -23,5 +26,16 @@ internal class PostsServiceTest (
         val result = postsService.findById(postsId)
 
         then(result).isEqualTo(PostsDomain(id = postsId, title = "제목", content = "내용", author = "나"))
+    }
+
+    @Test
+    internal fun `없는 아이디면 예외를 던진다`() {
+        // given
+        val randomPostsId = ThreadLocalRandom.current().nextLong(100)
+
+        // when & then
+        assertThrows<EntityNotFoundException> {
+            postsService.findById(randomPostsId)
+        }
     }
 }

5dbe642b72224e4645a0126b610eb675d57cb915
huisam <huisam@naver.com>
2021-05-02 14:05:27 +0900

    chore: p6spy 모듈 추가, query hint 제공

--- src/main/kotlin/com/huisam/kotlinweb/fegin/config/FeignClientConfiguration.kt
+++ src/main/kotlin/com/huisam/kotlinweb/fegin/config/FeignClientConfiguration.kt
@@ -6,10 +6,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.retry.annotation.EnableRetry
 
 @Configuration
-@EnableRetry
 @EnableFeignClients(basePackageClasses = [BaseFeignClientPackage::class])
 @ComponentScan(basePackageClasses = [BaseFeignClientPackage::class])
 class FeignClientConfiguration {

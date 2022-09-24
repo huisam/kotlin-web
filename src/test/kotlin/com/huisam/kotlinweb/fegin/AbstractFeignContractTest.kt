19048fb276abcfd701a65e8cf5b6336bc704e65b
huisam <huisam@naver.com>
2022-09-24 20:00:36 +0900

    test: exclude filter 추가

--- src/test/kotlin/com/huisam/kotlinweb/fegin/AbstractFeignContractTest.kt
+++ src/test/kotlin/com/huisam/kotlinweb/fegin/AbstractFeignContractTest.kt
@@ -3,13 +3,18 @@ package com.huisam.kotlinweb.fegin
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.huisam.kotlinweb.fegin.config.AutoConfigureTestFeign
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.TypeExcludeFilter
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
 import org.springframework.test.context.ContextConfiguration
 
 @TestConfiguration
-@ComponentScan(basePackageClasses = [FeignPackage::class])
+@ComponentScan(
+    basePackageClasses = [FeignPackage::class],
+    excludeFilters = [ComponentScan.Filter(type = FilterType.CUSTOM, classes = [TypeExcludeFilter::class])]
+)
 class FeignTestContextConfiguration
 
 @ContextConfiguration(classes = [FeignTestContextConfiguration::class])
--- src/test/kotlin/com/huisam/kotlinweb/persistence/AbstractPersistenceTest.kt
+++ src/test/kotlin/com/huisam/kotlinweb/persistence/AbstractPersistenceTest.kt
@@ -1,14 +1,19 @@
 package com.huisam.kotlinweb.persistence
 
+import org.springframework.boot.context.TypeExcludeFilter
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.transaction.annotation.Transactional
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
 
-@ComponentScan(basePackageClasses = [PersistencePackage::class])
+@ComponentScan(
+    basePackageClasses = [PersistencePackage::class],
+    excludeFilters = [ComponentScan.Filter(type = FilterType.CUSTOM, classes = [TypeExcludeFilter::class])]
+)
 @TestConfiguration
 class PersistenceTestContextConfiguration
 
